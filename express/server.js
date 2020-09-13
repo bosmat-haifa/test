d3e09db2ee3fca7fdf97960461c343b61bbcbc8e
bosmat-haifa <drdy.business@gmail.com>
2020-09-13 22:33:14 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -6,14 +6,17 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
+// router.get('/', (req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.write('<h1>Hello from Express.js!</h1>');
+//   res.end();
+// });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
-router.get('/signup', (req, res) => res.sendFile(path.join(__dirname, '../test/signup.html')));
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../test/signup.html'));
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
