9e5d167225ab20edc890252e63c2d0790488fd3c
bosmat-haifa <drdy.business@gmail.com>
2020-09-13 22:04:38 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -21,7 +21,8 @@ app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 app.use('/test', (req, res) => res.sendFile(path.join(__dirname, '../test/test.html')));
 
-app.use('*', (req, res) => {
+app.get('*', function(req, res) {
+  console.log("oof");
   res.status(404).sendFile(path.join(__dirname, "../statuses/404.html"));
 });
 
