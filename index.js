d1a3b645871ade86dd6285cd81021423db2f3f6d
bosmat-haifa <drdy.business@gmail.com>
2020-09-14 19:33:30 +0300

    g

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup.html')));
 
-app.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Local app listening on port ${PORT}!`));
