5048b50e81cb60f5af0f5f8e1a56f9458ed110a9
bosmat-haifa <drdy.business@gmail.com>
2020-09-14 21:10:29 +0300

    op

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup.html')));
 
-app.use((req, res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'pages/404/index.html'));
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "pages/404.html"));
  });
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Local app listening on port ${PORT}!`));
