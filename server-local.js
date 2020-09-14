9e503a6878a566c46d0060d8b082b56cb18346d9
bosmat-haifa <drdy.business@gmail.com>
2020-09-14 17:56:01 +0300

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -2,5 +2,5 @@
 
 const app = require('./express/server');
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Local app listening on port ${PORT}!`));
