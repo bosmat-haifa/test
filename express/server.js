0d8cd0287d710aef1ab6ffa930bbd92cba5f58d6
bosmat-haifa <drdy.business@gmail.com>
2020-09-14 18:11:11 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -6,11 +6,11 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-// router.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>what are you doing here</h1>');
-//   res.end();
-// });
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>what are you doing here</h1>');
+  res.end();
+});
 
 router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
@@ -20,8 +20,8 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 router.get('/signup', (req, res) => res.sendFile(path.join(__dirname, "../signupdark.html")));
 
 
-//app.use(bodyParser.json());
-app.use('/', router);//'/.netlify/functions/server', router);  // path must route to lambda
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
