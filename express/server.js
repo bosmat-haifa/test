596c83c0a0dd489a63da144c721a1451361fcdcf
bosmat-haifa <drdy.business@gmail.com>
2020-09-14 18:21:13 +0300

    j

--- express/server.js
+++ express/server.js
@@ -17,11 +17,6 @@ router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-router.get('/signup', (req, res) => {
-  res.writeHead(200, {"Content-Type": "text/html"});
-  res.sendFile(path.join(__dirname, "../signupdark.html"));
-  res.end();
-});
 
 
 app.use(bodyParser.json());
