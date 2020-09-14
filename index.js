16e2910abe173439140670a82a47635580e523e2
bosmat-haifa <drdy.business@gmail.com>
2020-09-14 20:09:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup.html')));
 
-app.use((req, res) => res.status(404).sendFile(path.join(__dirname, 'pages/404.haml')));
+app.use((req, res,next)=>{
+    res.status(404).send('<h1> Page not found </h1>');
+ });
 
 
 const PORT = process.env.PORT || 8080;
