f8c07dc686633b60d467caa8d89586e3d84ca2a7
bosmat-haifa <drdy.business@gmail.com>
2020-09-14 20:11:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup.html')));
 
 app.use((req, res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'pages/404.haml'));
+    res.sendFile(path.join(__dirname, 'pages/404.haml'));
  });
 
 
