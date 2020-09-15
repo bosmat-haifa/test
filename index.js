4d64c864f3c3611b6cfb3fa45fb093df34346c26
bosmat-haifa <drdy.business@gmail.com>
2020-09-15 19:10:31 +0300

    g

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup.html')));
+app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'pages/signupbeta.html')));
+app.get('/seventh-grade', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup/seventh-grade.html')));
+app.get('/ninth-grade', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup/ninth-grade.html')));
 
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, "pages/404.html"));
