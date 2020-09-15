29289a48f74618b01604682c9432edbe3588f2dd
bosmat-haifa <drdy.business@gmail.com>
2020-09-15 21:53:32 +0300

    jgjfg

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'pages/signupbeta.html')));
-app.get('/seventh-grade', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup/seventh-grade.html')));
-app.get('/ninth-grade', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup/ninth-grade.html')));
+app.get('/7th-grade', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup/7th-grade.html')));
+app.get('/10th-grade', (req, res) => res.sendFile(path.join(__dirname, 'pages/signup/10th-grade.html')));
 
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, "pages/404.html"));
